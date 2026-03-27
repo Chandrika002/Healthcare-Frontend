177938efd03435f52d886f2a45f9de9ad379dbcf
Chandrika002 <chandrika.csecu@gmail.com>
2026-03-27 22:59:09 +0600

    added admin_App.css, App.jsx, main.jsx

---
+++ src/admin_main.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./admin_App.jsx";
+
+export const Context = createContext({isAuthenticated: false});
+
+const AppWrapper = ()=>{
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
+  const [user, setUser] = useState(false);
+
+  return(
+    <Context.Provider value = {{isAuthenticated, setIsAuthenticated, user, setUser}}>
+      <App />
+    </Context.Provider>
+  );
+};
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <AppWrapper/>
+  </StrictMode>
+);
